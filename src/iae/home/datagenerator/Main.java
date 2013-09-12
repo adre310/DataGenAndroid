3c6d496ccb0e91a73769c4bdb61dfc3aa8d7b734
adre310 <adre310@gmail.com>
2013-09-12 16:38:09 +0400

    String Resources

--- src/iae/home/datagenerator/Main.java
+++ src/iae/home/datagenerator/Main.java
@@ -17,6 +17,7 @@ import org.simpleframework.xml.core.Persister;
 import iae.home.datagenerator.datamodel.DataModel;
 import iae.home.datagenerator.datamodel.FieldModel;
 import iae.home.datagenerator.datamodel.ObjectModel;
+import iae.home.datagenerator.datamodel.ResStrings;
 
 /**
  * @author aisaev
@@ -28,44 +29,17 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-/*		
-		ObjectModel om=new ObjectModel();
-		om.msClass="person";
-		om.msTable="person_table";
-		om.mFields=new ArrayList<FieldModel>();
-		
-		for(int i=0;i<4;i++){
-			FieldModel fm=new FieldModel();
-			fm.msName="field_"+Integer.toString(i);
-			fm.msColumn="col_"+Integer.toString(i);
-			om.mFields.add(fm);
-		}
-
-		AppSettings ap=new AppSettings();
-		ap.mProperties=new HashMap<String, String>();
-		ap.mProperties.put("1", "val 1");
-		ap.mProperties.put("2", "val 2");
-		ap.mProperties.put("3", "val 3");
-		
-		Serializer serializer = new Persister();
-		File result = new File("d:/temp/example.xml");
-		File settings=new File("d:/temp/settings.xml");
-		
-		try {
-		serializer.write(om, result);
-		serializer.write(ap, settings);
-		} catch (Exception e) {
-			System.out.println(e.getLocalizedMessage());
-		}
-*/
 
 		Serializer serializer = new Persister();
 		File result = new File("./res/model/datamodel.xml");
+		File resFile= new File("./res/strings.xml");
 		
 		try {
 			DataModel om=serializer.read(DataModel.class, result);
-			
-			
+
+			ResStrings resStrings=serializer.read(ResStrings.class, resFile);
+		
+			ExecStrings(resStrings, om);
 			//RuntimeSingleton.setProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, "d:\\temp");
 			Velocity.init();
 			Template tplClassBLL=null;
@@ -137,4 +111,39 @@ public class Main {
 		}
 	}
 
+	private static final String[] sExcludeFields=new String[] {
+		"Deleted",
+		"Modified",
+		"Guid"
+	};
+	
+	private static void ExecStrings(ResStrings resStrings,DataModel om) throws Exception {
+		Serializer serializer = new Persister();
+		File file=new File("D:/temp/out/strings.xml");
+		
+		for(ObjectModel obj : om.getObjects()) {
+			String keyObj=obj.getName().toLowerCase();
+			if(!resStrings.getStrings().containsKey(keyObj)) {
+				resStrings.getStrings().put(keyObj, obj.getName()+":");
+			}
+			
+			for(FieldModel fm : obj.getFields()) {
+				Boolean bFind=true;
+				
+				for(String s : sExcludeFields) {
+					if(s.equals(fm.getName()))
+							bFind=false;
+				}
+				
+				if(bFind) {
+					String key=(obj.getName()+"_"+fm.getName()).toLowerCase();
+					if(!resStrings.getStrings().containsKey(key)) {
+						resStrings.getStrings().put(key, fm.getName()+":");
+					}
+				}
+			}
+		}
+		
+		serializer.write(resStrings, file);
+	}
 }
